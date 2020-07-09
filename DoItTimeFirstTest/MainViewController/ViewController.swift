9d1c947484681d83a59f5c78f299e64a8bc6fc57
outsider1703 <simka-simka020697simka-simka1>
2020-07-09 13:18:29 +0200

    gz

--- DoItTimeFirstTest/MainViewController/ViewController.swift
+++ DoItTimeFirstTest/MainViewController/ViewController.swift
@@ -10,55 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var timer: Timer?
-    var timerCount = 0
-    
-    @IBOutlet var timeLabel: UILabel!
     @IBOutlet var cornerRadiusButtons: [UIButton]!
-    @IBOutlet var startButton: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        timeLabel.layer.cornerRadius = 15
         settingCornerRadiusForButtons()
     }
-    @IBAction func startTimeButton(_ sender: UIButton) {
-        timer = Timer.scheduledTimer(timeInterval: 1.0,
-                                     target: self,
-                                     selector: #selector(updateTimer),
-                                     userInfo: nil,
-                                     repeats: true)
-    }
     
-    @IBAction func stopTimerButton(_ sender: UIButton) {
-        timer?.invalidate()
-        timer = nil
-        timerCount = 0
-        timeLabel.text = "0"
-    }
-    
-    @objc func updateTimer() {
-        timerCount += 1
-        
-        let minuts = timerCount / 60
-        
-        
-        if timerCount < 60 {
-            timeLabel.text = String(timerCount)
-        } else {
-            timeLabel.text = "\(minuts):\(timerCount % 60)"
-        }
-    }
-}
-
-
-
-extension ViewController {
     private func settingCornerRadiusForButtons() {
         for button in cornerRadiusButtons {
             button.layer.cornerRadius = button.frame.width / 2
         }
-        startButton.layer.cornerRadius = startButton.frame.width / 2
     }
 }
